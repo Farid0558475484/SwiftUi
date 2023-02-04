a541d50a2b97f81df96c4efe48d768a892658ff6
Farid0558475484 <faridmh@code.edu.az>
2023-02-04 22:32:02 +0400

    ui first project

---
+++ MySwiftUi/MySwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUiApp.swift
+//  MySwiftUi
+//
+//  Created by Farid Hacizade on 04.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
