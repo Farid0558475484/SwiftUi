1ba96adf7356d788a8277d5308c9a7c520cc6a9b
Farid0558475484 <faridmh@code.edu.az>
2023-02-05 00:42:10 +0400

    work on the text

--- MySwiftUi/ContentView.swift
+++ MySwiftUi/ContentView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Image("apple")
-            Text("Apple")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .background(Color.blue)
-                .foregroundColor(Color.white)
+   
+            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+//            .lineLimit(1)
+//            .truncationMode(.tail)
+            .font(.largeTitle)
+                   .multilineTextAlignment(.center)
+            .background(Color.pink)
+            .foregroundColor(Color.white)
+        
+        
+        
+        
+//                .font(.largeTitle)
+//                .fontWeight(.bold)
+//                .background(Color.blue)
+//                .foregroundColor(Color.white)
             
             
-        }
+
     }
 }
 
